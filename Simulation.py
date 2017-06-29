Fixed negative resource levels bug, wrote payoff calculation inside harvest() method

Simulation.py
@@ -16,13 +16,17 @@ class Simulation(object):
         delta: float representing extent to which territories are ecologically
         connected
         q: float representing "catchability" of fish resource
+        price: price received per unit of fish
+        cost: cost per unit of effort invested in fishing
     Node (territory) attributes:
         R: float representing territory's resource level
         e: float representing territory's effort level
         dR: float, for internal use in calculating leakage.
+        pi: float representing territory's payoff from
+        current time step
     """
 
-    def __init__(self, n_fishers, delta, q, R_0, e_0):
+    def __init__(self, n_fishers, delta, q, R_0, e_0, price, cost):
         """Return a Simulation object with a complete graph on *n_fishers* nodes,
         a leakage factor of *delta*, a fish catchability factor of *q*, and
         individual nodes with *R_0[i]* resource level and *e_0[i]* effort level."""
@@ -32,6 +36,8 @@ class Simulation(object):
         self.t = 0 # intialize time step t to 0
         self.delta = delta
         self.q = q
+        self.price = price
+        self.cost = cost
         # TODO: check length of R_0 and e_0 arrays, throw error if wrong
         for i in range(self.G.number_of_nodes()):
             self.G.node[i]['R'] = R_0[i]
@@ -51,15 +57,17 @@ class Simulation(object):
         """Updates resource R for each territory based on e, the territory's
         effort level, for a single time step. Also calculates payoff from
         harvest."""
-        for i in range(self.G.number_of_nodes()):
-            R = self.G.node[i]['R']
-            e = self.G.node[i]['e']
-            R -= self.q * R * e
-            self.G.node[i]['R'] = R
-        # TODO: Calculate payoff (to be used in updating strategy) as a data
-        # attribute of the node. Later on this might be a numpy array if the
-        # strategy dynamic depends on the history of payoffs of the territory
-
+        for nood in self.G.nodes(data=False):
+            R = self.G.node[nood]['R']
+            e = self.G.node[nood]['e']
+            harvest = self.q * R * e
+            if harvest > R: # case 1: fisher wants more fish than he could take
+                harvest = R
+                self.G.node[nood]['R'] = 0
+            else: # case 2: fisher wants less fish than he could take
+                self.G.node[nood]['R'] = R - harvest
+            self.G.node[nood]['pi'] = self.price * harvest - self.cost * e
+            
     def leakage(self):
         """Updates resource R for each territory based on resource leakage
         between territories. Each territory i gives (delta * R / self.G.degree(i))
@@ -85,10 +93,14 @@ def main():
     """Performs unit testing."""
     R_0 = np.array([2.0,4.0,6.0])
     e_0 = np.array([1.0,2.0,3.0])
-    # TODO: how to ensure that the above are treated as floats and not ints?
-    my_sim = Simulation(3, 1, 1, R_0, e_0)
-    print("Node attributes before leakage: {}".format(my_sim.G.nodes(data=True)))
+    my_sim = Simulation(3, 1, 1, R_0, e_0, 1, 0.1)
+    print("Node attributes before harvest: {}".format(my_sim.G.nodes(data=True)))
+    my_sim.harvest()
+    for nood in my_sim.G.nodes(data=False):
+        print("Payoff for fisher {} is {}".format(nood, self.G.node[nood]['pi']))
+    print("Node attributes after harvest, before leakage: {}".format(my_sim.G.nodes(data=True)))
     my_sim.leakage()
     print("Node attributes after leakage: {}".format(my_sim.G.nodes(data=True)))
+
 if __name__ == "__main__":
     main()