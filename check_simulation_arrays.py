eba28e82e0812f448d74a97a4ac8d48bb0853e1b
Alice Lin <adlin@princeton.edu>
2017-08-02 22:53:19 -0400

    Got the 3D plot to work. Now to apply the same process to e vs. delta curve, varying parameters such as payoff_discount or num_feedback.

--- check_simulation_arrays.py
+++ check_simulation_arrays.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 from matplotlib.collections import LineCollection
+from matplotlib.collections import PolyCollection
 import matplotlib.colors as mcolors
 plt.switch_backend('Qt5Agg')
 
@@ -26,6 +27,14 @@ def e_avg_nodes(sim):
     e_avg = np.mean(sim.e_data, axis=0)
     return e_avg
 
+def cc(arg):
+    """Borrowed from an example from matplotlib"""
+    return mcolors.to_rgba(arg, alpha=0.3)
+
+def cc_lite(arg):
+    """Borrowed from an example from matplotlib"""
+    return mcolors.to_rgba(arg, alpha=0.2)
+
 if __name__ == '__main__':
     start_time = time.time()
 
@@ -43,8 +52,10 @@ if __name__ == '__main__':
     payoff_discount = 0.5
     num_steps = 1000
 
-    deltas = np.linspace(0,1,10,endpoint=True)
+    # Assign delta values
+    deltas = np.linspace(0, 0.1, 10, endpoint=True)
 
+    # Run simulations at different delta values to determine effort values
     e_vals = np.zeros((deltas.size, num_steps))
     for i in range(deltas.size):
         sim = Simulation_arrays.Simulation_arrays(n_fishers, deltas[i], q, r, K, R_0,
@@ -52,21 +63,40 @@ if __name__ == '__main__':
         sim.simulate()
         e_vals[i] = e_avg_nodes(sim)
 
-    colors = np.linspace(0,1,deltas.size,endpoint=False)
-
-    for i in range(deltas.size):
-        plt.plot(np.arange(num_steps), e_vals[i], color=(1, colors[i], colors[i]))
-
+    # Calculate analytical e_MSR and e_Nash effort levels
     e_msr = calculate_e_msr(n_fishers, q, r, K, price, cost)
     e_nash = calculate_e_nash(e_msr, n_fishers)
+
+    # Plotting stuff
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    xs = np.arange(num_steps)
+    ys = e_vals
+    ys[:, 0] = e_msr * 0.8 # ensures face color extends to bottom of plot
+    ys[:, -1] = e_msr * 0.8
+    zs = deltas
+    verts = []
+    for i in range(zs.size):
+        verts.append(list(zip(xs, ys[i])))
     
-    plt.axhline(y=e_msr, ls='--', c='c', linewidth=1)
-    plt.axhline(y=e_nash, ls='--', c='m', linewidth=1)
+    colors = [cc('r') for i in range(deltas.size)]
+    lines = LineCollection(verts, linewidth=0.5, facecolors=colors)
+    ax.add_collection3d(lines, zs=zs, zdir='y')
 
-    plt.text(0.1, e_msr, "$e_{MSR}$", va='bottom')
-    plt.text(0.1, e_nash, "$e_{Nash}$", va='top')
+    # Making horizontal planes for the e_nash and e_msr effort levels
+    plane = [(0,deltas[0]), (0.0001, deltas[-1]),
+             (num_steps - 0.001, deltas[-1]), (num_steps, deltas[0])]
+    horizs = [plane, plane]
+    planes = PolyCollection(horizs, facecolors=[cc_lite('b'), cc_lite('b')])
+    ax.add_collection3d(planes, zs=[e_msr, e_nash], zdir='z')
 
-    plt.xlabel("Time step")
-    plt.ylabel("Effort")
+    ax.set_xlabel("Time step")
+    ax.set_ylabel("Deltas")
+    ax.set_zlabel("Effort Level")
+    ax.set_xlim3d(0, num_steps)
+    ax.set_ylim3d(0,deltas[-1])
+    ax.set_zlim3d(e_msr * 0.8, e_nash * 1.2)
+
     print("--- %s seconds ---" % (time.time() - start_time))
     plt.show()
