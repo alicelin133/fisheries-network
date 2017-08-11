cc67ffac9d143cc914b0dba087cf87853832672e
Alice Lin <adlin@princeton.edu>
2017-08-10 21:10:07 -0400

    Added the horizontal lines representing e_msr and e_nash.

--- plot_lattice_e_vs_delta.py
+++ plot_lattice_e_vs_delta.py
@@ -21,6 +21,7 @@ def get_vals(raw):
     return data
 
 def main():
+    # Get values to plot
     e_raw = np.load("/Users/alicelin/Documents/fish/fisheries-network" + \
         "/data/e-lattice-4x4-num_deltas21-deltas0_0to0_1-q1-r0_05-K20" + \
         "0-price1-cost0_5-noise0_0001-R_0100_0-num_feedback25-num_ste" + \
@@ -35,6 +36,9 @@ def main():
         "um_steps1000-v0.npy")
     es = get_vals(e_raw)
     Rs = get_vals(R_raw)
+
+    # Calculae e_msr, e_nash to plot as hlines
+    e_msr, e_nash = lattice_e_vs_delta.get_e_msr_nash(16, 0.05, 1, 200, 1, 0.5)
     
     # Making figure
     plt.style.use('bmh')
@@ -48,14 +52,22 @@ def main():
     ax1.set_xticks(np.linspace(deltas[0],deltas[-1],11))
     ax1.set_xlabel("$\delta$")
     ax1.set_ylabel("Equilibrium effort", color='b')
-    ax1.set_title("Effort vs. $\delta$ on a 4-by-4 lattice")
+    ax1.set_title("Effort, resource vs. $\delta$ on a 4-by-4 lattice")
     ax1.tick_params('y', colors='b')
+    ax1.grid(b=False)
+
+    # Plotting e_msr, e_nash as hlines
+    ax1.axhline(y=e_msr, linestyle='--', color='c', linewidth=0.5)
+    ax1.axhline(y=e_nash, linestyle='--', color='c', linewidth=0.5)
+    ax1.text(0, e_msr * 1.01, '$e_{MSR}$', va='bottom', color='c')
+    ax1.text(0.09, e_nash * 0.99, '$e_{Nash}$', va='top', color='c')
 
     # Plotting resource level vs. delta
     ax2 = ax1.twinx()
     ax2.plot(deltas, Rs, 'r', marker='o', markevery=1, markersize=5, linewidth=0.5)
     ax2.set_ylabel("Equilibrium resource level", color='r')
     ax2.tick_params('y', colors='r')
+    ax2.grid(b=False)
 
     plt.show()
     
