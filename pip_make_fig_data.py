4376bd6ec56ce981a50e2011480215d442ca75a2
Alice Lin <adlin@princeton.edu>
2018-02-01 17:43:50 -0500

    Changed the formatting of the txt files saved, also added horizontal lines on the plot demarcating e_msr and e_nash.

--- pip_make_fig_data.py
+++ pip_make_fig_data.py
@@ -11,19 +11,23 @@ plt.switch_backend('Qt5Agg')
 
 import time
 
-def plot_e_vs_d(deltas, e_list):
+def plot_e_vs_d(deltas, e_list, e_msr, e_nash):
     """Given the numpy array *deltas* and the list *e_list*, makes a plot."""
     fig, ax = plt.subplots()
     ax.plot(deltas, e_list)
     ax.set_xlabel(r'$\delta$')
     ax.set_ylabel('Stable effort level')
     ax.set_title(r'ESS vs. $\delta$')
+    ax.axhline(y=e_msr, linestyle='--', color='c', linewidth=0.5)
+    ax.axhline(y=e_nash, linestyle='--', color='c', linewidth=0.5)
+    ax.text(deltas[-1], e_msr * 1.01, '$e_{MSR}$', va='bottom', ha='right', color='c')
+    ax.text(deltas[0], e_nash * 0.99, '$e_{Nash}$', va='top', color='c')
 
 def main():
     e_list = []
     matrix_list = []
 
-    # Set parameters other than delta
+    # set parameters other than delta
     m = 6
     n = 6
     delta = 0 # will be changed later
@@ -41,30 +45,35 @@ def main():
               'e_0': e_0, 'p': p, 'w': w, 'num_feedback': num_feedback,
               'copy_noise': copy_noise, 'gm': gm, 'num_steps': num_steps}
     
-    num_levels = 40
+    num_levels = 20
 
-    # delta values to be tested
-    num_deltas = 3
-    deltas = np.linspace(0, 0.1, num=num_deltas)
+    # choose delta values to be tested
+    num_deltas = 2
+    start = 0
+    end = 0.005
+    deltas = np.linspace(start, end, num=num_deltas, endpoint=False)
 
+    # generate data
     for delta in deltas:
         params['delta'] = delta
         pip1 = Pip.Pip(params, num_levels)
+        e_msr = pip1.e_msr
+        e_nash = pip1.e_nash
         pip1.create_matrix()
         e_list.append(pip1.get_eq())
         matrix_list.append(pip1.get_matrix())
     
     # save *e_list* and each matrix in *matrix_list*
-    path = '/Users/alicelin/Documents/fish/fisheries-network/data/test-save/'
-    e_list_fname = path + 'd' + str(deltas[0]).split('.')[1] + 'to' +
-                    str(deltas[-1]).split('.')[1] + 'e_list'
-    np.savetxt(e_list_fname, e_list)
+    path = '/Users/alicelin/Documents/fish/fisheries-network/Pairwise Invasibility Plots/80x80_data/'
+    e_list_fname = path + 'd' + str(start).split('.')[-1] + 'to' + \
+                    str(end).split('.')[-1] + 'e_list'
+    np.savetxt(e_list_fname, e_list, fmt='%9.8f')
     for i in range(len(matrix_list)):
         matrix_fname = path + 'd' + str(deltas[i]).split('.')[1] + 'matrix'
-        np.savetxt(matrix_fname, matrix_list[i])
+        np.savetxt(matrix_fname, matrix_list[i], fmt='%d')
 
     # plot effort vs delta
-    plot_e_vs_d(deltas, e_list)
+    plot_e_vs_d(deltas, e_list, e_msr, e_nash)
 
 if __name__ == '__main__':
     start = time.time()
