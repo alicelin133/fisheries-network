b4ea23953fb6c9fd4b232225a62ad6785cb4c9ab
Alice Lin <adlin@princeton.edu>
2018-08-30 19:09:47 -0400

    1. Changed the error with the well-mixed case for leakage(). 2. In leakage, now delta/N of resource goes to each neighbor instead of delta/(N+1) as before. 3. order of harvest/regrowth has been altered to match the new difference equation.

--- Sim_no_update.py
+++ Sim_no_update.py
@@ -38,8 +38,8 @@ class Sim_no_update(object):
         wellmixed = are the fish well-mixed in their movement?
         R_t = mxn numpy array, current resource levels for each fisher
         e_t = mxn numpy array, current effort levels for each fisher
-        R_data = mxnxnum_steps np array, saves R_t for all t
-        e_data = mxnxnum_steps np array, saves e_t for all t
+        R_data = num_stepsxmxn np array, saves R_t for all t
+        e_data = num_stepsxmxn np array, saves e_t for all t
         neighbors = mxnxDEGREEx2 np array, index [i][j] returns fisher's 4
             neighbor coordinates
     """
@@ -62,6 +62,8 @@ class Sim_no_update(object):
         self.gm = params['gm']
         self.num_steps = params['num_steps']
         self.wellmixed = params['wellmixed'] # whether the fish are well-mixed
+        if self.wellmixed:
+            self.DEGREE = self.m * self.n - 1 # fish can move between any two fishers
         self.R_t = np.copy(params['R_0'])
         self.e_t = np.copy(params['e_0'])
         self.pi_t = np.zeros((self.m,self.n))
@@ -69,7 +71,8 @@ class Sim_no_update(object):
         self.e_data = np.zeros((self.num_steps, self.m, self.n))
         self.pi_data = np.zeros((self.num_steps, self.m, self.n))
         self.neighbors = np.zeros((self.m, self.n, self.DEGREE, 2), dtype=int)
-        self.get_neighbors()
+        if not self.wellmixed:
+            self.get_neighbors() # this fn is not used in the well-mixed case
         self.dR = np.zeros((self.m, self.n)) # used in leakage()
         self.R_from_neighbors = np.zeros((self.m, self.n)) # used in leakage()
 
@@ -115,44 +118,50 @@ class Sim_no_update(object):
         for i in range(self.num_feedback):
             self.harvest()
             self.regrowth()
+            self.R_t = self.R_t - self.harvested + self.babies
             self.leakage()
 
     def harvest(self):
-        """Updates R_t based on the efforts e_t of the fishers.
+        """Calculates how much fish is harvested.
         Also calculates payoffs for each fisher."""
-        harvest = self.R_t * self.q * self.e_t
-        self.R_t = self.R_t - harvest
-        self.get_payoff(harvest)
+        self.harvested = np.copy(self.R_t * self.q * self.e_t)
+        # self.R_t = self.R_t - harvest
+        self.get_payoff()
 
-    def get_payoff(self, harvest):
+    def get_payoff(self):
         """Calculates payoff for each fisher, where pi = (pqR - w)e"""
-        self.pi_t = self.p * harvest - self.w * self.e_t
+        self.pi_t = self.p * self.harvested - self.w * self.e_t
 
     def regrowth(self):
-        """Updates R_t based on the logistic growth of fish."""
-        self.R_t = self.R_t * (1 + self.r * (1 - self.R_t))
+        """Calculates amount of fish born in current time step."""
+        self.babies = np.copy(self.R_t * self.r * (1 - self.R_t))
 
     def leakage(self):
-        """Updates R_t based on fish movement across edges of the lattice."""
-        if self.wellmixed:
+        """Updates R_t based on fish movement across edges of the lattice.
+        IMPORTANT: each fisher gives delta/#neighbors of their resource to
+        each of their neighbors, losing a total of delta of their resource."""
+        if self.wellmixed: # TODO: NEED TO FIX
             # Fish are well-mixed
-            # Total resources are evenly divided among all fishers
-            fishpercap = np.sum(self.R_t) / (self.m * self.n)
-            self.R_t = np.full((self.m, self.n), fishpercap)
+            total_R = np.sum(self.R_t)
+            for i in range(self.m):
+                for j in range(self.n):
+                    # each fisher receives 1/(mn - 1) * (total fish - their own fish) from nbrs
+                    self.R_from_neighbors[i,j] = self.delta * (total_R - self.R_t[i,j]) / \
+                                                 (self.m * self.n - 1)
         else:
             # Fish can only move along edges of lattice
-            self.dR = self.delta * self.R_t / (self.DEGREE + 1)
             # ^how much each fisher is giving to one of their neighbors
+            self.dR = self.delta * self.R_t / self.DEGREE
             for i in range(self.m):
                for j in range(self.n):
                    n = self.neighbors[i][j]
                    dR = self.dR[i][j] # how much to give to each neighbor
                    for k in range(self.DEGREE):
-                       # each neighbor of (i,j) fisher gets dR from the (i,j) fisher
+                       # each neighbor of (i,j)th fisher gets dR from him
                        self.R_from_neighbors[n[k][0],n[k][1]] += dR
                        # TODO: NEED TO TEST THIS
-            self.R_t = self.R_t - self.DEGREE * self.dR + self.R_from_neighbors
-            self.R_from_neighbors = np.zeros((self.m,self.n)) # reset this to zeros
+        self.R_t = (1 - self.delta) * self.R_t + self.R_from_neighbors
+        self.R_from_neighbors = np.zeros((self.m,self.n)) # reset this to zeros
 
 def calculate_e_msr(m, n, q, r, price, cost):
     """Calculates value of e_msr (maximum sustainable rent)."""
@@ -170,23 +179,23 @@ def main():
     seed = 17
     np.random.seed(seed)
     # set parameter values
-    m = 100
-    n = 1
-    delta = 1
+    m = 6
+    n = 6
+    delta = 0.5
     q = 1
     r = 0.2
     R_0 = np.full((m, n), 0.5)
     p = 1
     w = 0.5
-    num_feedback = 15
+    num_feedback = 5
     copy_noise = 0.0005 # not actually used here
     gm = False
-    num_steps = 51
-    wellmixed = True # False means that the fish move along the lattice i.e. not well-mixed
+    num_steps = 200
+    wellmixed = False # False means that the fish move along the lattice i.e. not well-mixed
     # creating e_0
     e_msr = calculate_e_msr(m, n, q, r, p, w)
     e_nash = calculate_e_nash(e_msr, m, n)
-    e_0 = np.linspace(0, e_nash, num=m * n)
+    e_0 = np.linspace(0, r/q, num=m * n)
     e_0 = e_0.reshape((m,n))
     print("e_msr: {}".format(e_msr))
     print("e_nash: {}".format(e_nash))
@@ -199,6 +208,10 @@ def main():
     mysim = Sim_no_update(params)
     mysim.run_sim()
     print("avg e: {}".format(np.mean(mysim.e_data[50:])))
+    R_res_eq = mysim.R_data[-1,:,:]
+    print(R_res_eq.shape)
+    #print('Final resource level: {}'.format(R_res_eq))
+    print('Final avg resource level: {}'.format(np.mean(R_res_eq)))
     fig = plt.figure()
     ax1 = fig.add_subplot(2,1,1)
     ax1.plot(np.arange(num_steps), np.average(mysim.e_data, axis=(1,2)))
